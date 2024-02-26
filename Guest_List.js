8f025b5f4c4aded7357af4e9f1d7fdf28c3aeefa
imanwaheed317 <160356928+imanwaheed317@users.noreply.github.com>
2024-02-26 21:31:47 +0500

    Add files via upload

---
+++ Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['IMRAN KHAN', 'NAWAZ SHARIF', 'SHABAZ SHARIF', 'ASIF ALI ZARDARI'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear mr. ' + Guest_List[i] + ',\n\n It is our pleasure to invite you in our party. \n\n Thank you!');
+}
---
+++ Guest_List.ts
@@ -0,0 +1,6 @@
+let Guest_List :string[] = ['IMRAN KHAN', 'NAWAZ SHARIF', 'SHABAZ SHARIF', 'ASIF ALI ZARDARI'];
+
+for(let i=0; i<Guest_List. length; i++){
+
+    console.log('Dear mr. ' + Guest_List[i] + ',\n\n It is our pleasure to invite you in our party. \n\n Thank you!')
+}
